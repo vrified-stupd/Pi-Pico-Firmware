7b12ee84084974656f8c3840d2b99242e78a1875
lavahead <123059883+vrified-stupd@users.noreply.github.com>
2023-12-13 17:16:41 +0800

    Update firmware.py
    - Added boot logs to see why its crashing when booting
    - Added a bunch more functions that are mostly unfinished

--- firmware.py
+++ firmware.py
@@ -10,15 +10,52 @@ class main():
     I2C_ADDR     = 0x27
     I2C_NUM_ROWS = 4
     I2C_NUM_COLS = 20
+    #END
+    
+    #Important Functions (DONT DELETE)
+    def runFile():
+        code.Main()
+
+    def crash():
+        print("This program has crashed!, please check the crash logs to see why")
+        lcd.putstr("CRASHED! CHECK LOGS")
+
+    def bootLogs():
+        #LOG FILE BEGIN
+        logsFile = open("logs.txt", "w")
+        logsFile.write("")
+        logsFile.close()
+        logsFile = open("logs.txt", "a")
+        logsFile.write("Booted! \n")
+        logsFile.write("Checking for code.py file... \n") #Checks for the coding file
+        logsFile.close()
+        codeFile = open("code.py" "a")
+        codeFile.close()
+        logsFile = open("logs.txt", "a")
+        logsFile.write("code.py file exists. \n")
+        logsFile.write("Checking for LCD files... \n") #Checks for LCD files
+        logsFile.close()
+        lcdFile = open("lcd_api.py", "a")
+        lcdFile.close()
+        lcdFile = open("pico_i2c_lcd.py", "a")
+        lcdFile.close()
+        logsFile = open("logs.txt", "a")
+        logsFile.write("LCD files exist. \n")
+        logsFile.write("Boot Logging all done! Proceeding to program... \n")
+        #END
+        
+    def crashLogs():
+        print("") #THIS IS NOT YET FINISHED
+        
+    #END
     
     def boot():
         #put variables here
         hi = "Hello, World!"
         i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
         lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
-        
-        def runFile():
-            code.Main()
+
+        bootLogs()
         
         #END
         print("boot")
@@ -26,8 +63,10 @@ class main():
         def code():
             while True:
                 utime.sleep(1)
+                #You can put anything here
                 print(hi)
                 lcd.putstr("Hello")
+                #Just dont delete the runFile(). Thats where you code
                 #Run File
                 runFile()
                 #END
